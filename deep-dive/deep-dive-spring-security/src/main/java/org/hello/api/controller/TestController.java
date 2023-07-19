c8f9693bac5ed66de8b31bc9833d1c5bf2e42ae4
Dave <dave.ran.jin@kingland.com>
2023-07-19 16:55:35 +0800

    Test API versioning

--- deep-dive/deep-dive-spring-security/src/main/java/org/hello/api/controller/TestController.java
+++ deep-dive/deep-dive-spring-security/src/main/java/org/hello/api/controller/TestController.java
@@ -3,22 +3,40 @@ package org.hello.api.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    /**
+     * @deprecated Test deprecated annotation
+     */
     @GetMapping("/test")
     @ResponseBody
     @Deprecated(since = "release-1.2.0", forRemoval = true)
-    public String test() { // Test
+    public String test() { //
         return "test";
     }
 
     @GetMapping(value = "/test", headers = { "X-API-VERSION=v2" })
     public ResponseEntity<String> testV2() {
         return ResponseEntity.ok().body("v2");
+    }
+
+    @GetMapping("/test?version=1.3.0")
+    public String testV3(
+            @RequestParam(value = "version", required = true) String version
+    ) {
+        return "v3";
+    }
+
+    @GetMapping(value = "/test", params = "version=1.3.0")
+    public String testV4(
+            @RequestParam(value = "version", required = true) String version
+    ) {
+        return "v4";
     }
 
     @GetMapping("/test-auth")
